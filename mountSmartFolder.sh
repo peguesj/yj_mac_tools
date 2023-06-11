c9dbf4b92aa60309631cba6bc60aedd16736f0c7
Jeremiah Pegues <jeremiah.pegues@gmail.com>
2023-06-11 22:58:01 +0700

    init mountsmartfolder

--- mountSmartFolder.sh
+++ mountSmartFolder.sh
@@ -21,15 +21,28 @@ if [ -d "$MOUNT_POINT" ]; then
     exit 0
 fi
 
-# Create the mount point directory
-mkdir "$MOUNT_POINT"
+# Create the mount point directory if it does not exist
+if [ ! -d "$MOUNT_POINT" ]; then
+    mkdir "$MOUNT_POINT"
+    if [ $? -ne 0 ]; then
+        echo "Failed to create the mount point directory: $MOUNT_POINT"
+        exit 1
+    fi
+fi
 
-# Mount the smart folder as a drive
-mount_afp -i "afp://localhost$SMART_FOLDER" "$MOUNT_POINT"
+# Mount the smart folder as a drive using SMB
+mount_smbfs "$SMART_FOLDER" "$MOUNT_POINT"
 
 # Check if the mount operation was successful
 if [ $? -eq 0 ]; then
     echo "Smart folder mounted successfully at $MOUNT_POINT"
+    
+    # Create symbolic links to the contents of the smart folder
+    for item in "$SMART_FOLDER"/*; do
+        ln -s "$item" "$MOUNT_POINT/$(basename "$item")"
+    done
+    
+    echo "Symbolic links created to the contents of the smart folder"
 else
     echo "Failed to mount the smart folder"
     # Clean up the created mount point directory
