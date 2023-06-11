92f3146159a23c5550c5935c287598de13f73222
Jeremiah Pegues <jeremiah.pegues@gmail.com>
2023-06-11 23:03:33 +0700

    added logging and status for STDOUT

--- mountSmartFolder.sh
+++ mountSmartFolder.sh
@@ -15,6 +15,9 @@ if [ -z "$SMART_FOLDER" ] || [ -z "$MOUNT_POINT" ]; then
     exit 1
 fi
 
+# Expand the tilde symbol to the user's home directory
+SMART_FOLDER="${SMART_FOLDER/#\~/$HOME}"
+
 # Check if the smart folder is already mounted
 if [ -d "$MOUNT_POINT" ]; then
     echo "Smart folder is already mounted at $MOUNT_POINT"
@@ -30,19 +33,48 @@ if [ ! -d "$MOUNT_POINT" ]; then
     fi
 fi
 
-# Open the smart folder to automatically mount it
-open -a Finder "$SMART_FOLDER"
+# Get the number of files to be created
+file_count=$(find "$SMART_FOLDER" -type f | wc -l)
+echo "Number of files to be created: $file_count"
 
-# Wait for the smart folder to be mounted
-while [ ! -d "$MOUNT_POINT" ]; do
-    sleep 1
-done
+# Mount the smart folder as a drive
+mount -t smbfs "$SMART_FOLDER" "$MOUNT_POINT"
 
-echo "Smart folder mounted successfully at $MOUNT_POINT"
-
-# Create symbolic links to the contents of the smart folder
-for item in "$SMART_FOLDER"/*; do
-    ln -s "$item" "$MOUNT_POINT/$(basename "$item")"
-done
-
-echo "Symbolic links created to the contents of the smart folder"
+# Check if the mount operation was successful
+if [ $? -eq 0 ]; then
+    echo "Smart folder mounted successfully at $MOUNT_POINT"
+    
+    # Create symbolic links to the contents of the smart folder
+    file_index=0
+    total_progress=50
+    
+    for item in "$SMART_FOLDER"/*; do
+        item_basename=$(basename "$item")
+        
+        # Determine the most compatible link type based on the item's source
+        link_type=""
+        if [[ -f "$item" ]]; then
+            link_type="Hard Link"
+        elif [[ -d "$item" ]]; then
+            link_type="Symbolic Link"
+        fi
+        
+        # Display progress and verbose messaging
+        ((file_index++))
+        progress=$((file_index * total_progress / file_count))
+        echo -ne "[$progress%] Creating $link_type: $item_basename...\r"
+        
+        # Create the appropriate type of link
+        if [[ -f "$item" ]]; then
+            ln "$item" "$MOUNT_POINT/$item_basename"
+        elif [[ -d "$item" ]]; then
+            ln -s "$item" "$MOUNT_POINT/$item_basename"
+        fi
+    done
+    
+    echo -e "\nSymbolic links created to the contents of the smart folder"
+else
+    echo "Failed to mount the smart folder"
+    # Clean up the created mount point directory
+    rmdir "$MOUNT_POINT"
+fi
